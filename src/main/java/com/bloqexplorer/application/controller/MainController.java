b209bcab44649a83be94d813e794460c2fc87a77
AndrewKharyshyn <renegaden@ua.fm>
2018-10-03 11:22:30 +0300

    Initial commit

---
+++ src/main/java/com/bloqexplorer/application/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.bloqexplorer.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("eventName", "FIFA 2018");
+
+        return "index";
+    }
+}
